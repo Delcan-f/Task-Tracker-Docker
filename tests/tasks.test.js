ad867514d1bf0507eacbbda4a32c053a8ce30819
Delcan-f <109348946+Delcan-f@users.noreply.github.com>
2025-05-04 01:29:41 +1000

    feat: docker

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 
 const app = require("../src/index");
 
+jest.setTimeout(10000);
+
 describe(`GET /health`, () => {
     it(`should return stats OK`, async() => {
         const response = await request(app).get('/health');
@@ -10,3 +12,25 @@ describe(`GET /health`, () => {
     });
 });
 
+describe(`GET /tasks`, () => {
+    it(`should return all tasks`, async () => {
+        const response = await request(app).get('/tasks');
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
+});
+
+describe('POST /tasks', () => {
+    it('should create a new task', async () => {
+        const newTask = {
+            title: 'Test Task',
+            description: 'Test description',
+            status: 'in-progress'
+        };
+        
+        const response = await request(app).post('/tasks').send(newTask);
+        
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toMatchObject(newTask);
+    });
+});
